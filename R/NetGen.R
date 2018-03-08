9d4dd4a54437d1ac16a58caeefb055d29593c7e7
Carl Boettiger <cboettig@gmail.com>
2018-03-08 17:56:03 +0000

    some documentation

--- R/NetGen.R
+++ R/NetGen.R
@@ -1,8 +1,26 @@
-
 #' netgen
 #'
 #' netgen function
 #'
+#' @param name a user-specified name for this network
+#' @param n_modav network size and average module size (integer vector, length 2)
+#' @param cutoffs module and submodule minimum sizes (integer vector, length 2).
+#'  (submodules are used only for bipartite and tripartite networks)
+#' @param net_type integer indicating type, see details
+#' @param net_degree average degree of connection
+#' @param net_rewire global and local  network rewiring probabilities
+#' @param mod_probs module probabilities for types 1 to 51,
+#'   used for constructing mixed networks, net_type = 0
+#' @details
+#' network type
+#' 0 = mixed
+#' 1 = random
+#' 2 = scalefree
+#' 3 = nested
+#' 41 = bi-partite nested
+#' 42 = bi-partite random
+#' 51 = tri-trophic bipartite nested-random
+#' 52 = tri-trophic bipartite nested-bipartite nested
 #' @importFrom stats runif
 #' @useDynLib NetGen
 #' @export
--- R/star.R
+++ R/star.R
@@ -1,6 +1,6 @@
 # script to test the link between Fortran and R
 
-#' @export
+
 star = function(a,b){
   ## res holds the results for the Fortran call
   res =.Fortran("multiply",as.integer(a),as.integer(b),c=integer(1))
