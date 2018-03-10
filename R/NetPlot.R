c71d1cb84b7873e3097aa23a660f544593b7f9f2
Carl Boettiger <cboettig@gmail.com>
2018-03-09 22:53:58 -0800

    update

--- R/NetPlot.R
+++ R/NetPlot.R
@@ -1,31 +1,20 @@
 # https://matthewlincoln.net/2014/12/20/adjacency-matrix-plots-with-r-and-ggplot2.html
 
+#' Plot network adjacency matrix
+#'
+#' @param graph an igraph object
 #' @export
 #' @import ggplot2
-#' @importFrom dplyr mutate
 #' @importFrom igraph graph.data.frame get.data.frame
 #' @examples
-#' M <- netgen()
-#' netplot(M)
+#' graph <- netgen()
+#' adj_plot(graph)
 #'
-#' netplot(netgen(net_type=3))
-netplot <- function(M){
-  graph <- graph.data.frame(M)
+adj_plot <- function(graph){
 
-  # Re-generate dataframes for both nodes and edges, now containing
-  # calculated network attributes
-  node_list <- get.data.frame(graph, what = "vertices")
-  edge_list <- get.data.frame(graph, what = "edges")
-  # Create a character vector containing every node name
-  all_nodes <- sort(node_list$name)
+  edge_list <- igraph::get.data.frame(graph, what = "edges")
 
-# Adjust the 'to' and 'from' factor levels so they are equal
-# to this complete list of node names
-  plot_data <- mutate(edge_list,
-                    to = factor(to, levels = all_nodes),
-                    from = factor(from, levels = all_nodes))
-
-  ggplot(plot_data, aes(x = from, y = to)) +
+  ggplot(edge_list, aes(x = from, y = to)) +
       geom_raster() + theme_bw() +
       scale_x_discrete(drop = FALSE) +
       scale_y_discrete(drop = FALSE) +
@@ -34,7 +23,10 @@ netplot <- function(M){
       theme(
         axis.text = element_blank(),
         aspect.ratio = 1,
-        legend.position = "none")
+        legend.position = "none",
+        panel.grid.major = element_blank(),
+        panel.grid.minor = element_blank(),
+        axis.ticks = element_blank())
 }
 
 
